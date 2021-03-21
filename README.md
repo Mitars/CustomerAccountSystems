cff8fff6d2e59541d616ce9582becf97e7ecf20d
Mitar Stjepcevic <Mitarkun@gmail.com>
2021-03-21 20:51:02 +0100

    Add customer project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CurrentAccountSystems
+# Current Account Systems
 Allows management of customer accounts and transactions
---
+++ customer/Cas.Customer.Api/DataAccess/CustomerDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using Cas.Customer.Api.Models;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Cas.Customer.Api.DataAccess
+{
+    /// <summary>
+    /// The customer repository class.
+    /// Reads the customer information from a JSON file.
+    /// </summary>
+    public class CustomerDataAccess
+    {
+        private readonly JsonSerializerOptions jsonSerializerOptions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerDataAccess"/> class.
+        /// </summary>
+        public CustomerDataAccess()
+        {
+            this.jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+        }
+
+        /// <summary>
+        /// Gets all the customers.
+        /// </summary>
+        /// <returns>The list of all customers.</returns>
+        public List<CustomerModel> Get()
+        {
+            var json = System.IO.File.ReadAllText(@"DataAccess\customerData.json");
+            return JsonSerializer.Deserialize<List<CustomerModel>>(json, jsonSerializerOptions);
+        }
+
+        /// <summary>
+        /// Gets a customer by their ID.
+        /// </summary>
+        /// <param name="id">The customer ID.</param>
+        /// <returns>The customer.</returns>
+        public CustomerModel Get(long id) =>
+            this.Get().Find(customer => customer.Id == id);
+    }
+}
